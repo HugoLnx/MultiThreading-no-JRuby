5050b3dc7f9e4894af215dd0f7510be87bb00ab5
HugoLnx <hugolnx@gmail.com>
2010-07-11 22:05:30 -0700

    criada a classe Consumidor

---
+++ lib/consumidor.rb
@@ -0,0 +1,23 @@
+module MultiThreading
+	class Consumidor
+		include Runnable
+		
+		def initialize(localizacao_compartilhada)
+			@localizacao_compartilhada = localizacao_compartilhada
+		end
+		
+		def run
+			soma = 0
+			9.times do
+				begin
+					JThread.sleep rand(3000)
+					soma += @localizacao_compartilhada.get
+				rescue InterruptedException => e
+					e.print_stack_trace
+				end
+			end
+			puts "Fim do Consumidor\n" +
+			     "Valor da Soma: #{soma}"
+		end
+	end
+end
---
+++ spec/consumidor_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+module MultiThreading
+	UmBuffer = Class.new{include Buffer}
+	
+	describe 'Um Consumidor' do
+		before :all do
+			@buffer = UmBuffer.new
+		end
+	
+		it 'deveria implementar Runnable' do
+			Consumidor.should include Runnable
+		end
+	
+		it 'deveria ser instanciavel' do
+			lambda{Consumidor.new @buffer}.should_not raise_error
+		end
+		
+		context 'apos ser instanciado' do
+			before :all do
+				@consumidor = Consumidor.new @buffer
+			end
+			
+			it 'deveria ter uma localizacao compartilhada' do
+				localizacao_compartilhada = @consumidor.instance_variable_get :@localizacao_compartilhada
+				localizacao_compartilhada.should_not be_nil
+			end
+			
+			it 'deveria ser executavel' do
+				lambda{@consumidor.run}.should_not raise_error NoMethodError
+			end
+		end
+	end
+end
